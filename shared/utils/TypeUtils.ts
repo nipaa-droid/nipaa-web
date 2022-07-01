006735bbf2f09ea61fb9b0e6cf251ca642e7e187
o-dasher <nipaa_droid@proton.me>
2022-07-01 17:30:09 -0400

    Adds tuple and NonEmptyArray types

--- shared/utils/TypeUtils.ts
+++ shared/utils/TypeUtils.ts
@@ -3,3 +3,13 @@ export type NonNullableKeys<T, K extends (keyof T)[]> = {
 } & {
   [P in K[number]]-?: NonNullable<T[P]>;
 };
+
+export type Tuple<
+  T,
+  N extends number,
+  R extends readonly T[] = []
+> = R["length"] extends N ? R : Tuple<T, N, readonly [T, ...R]>;
+
+export type NonEmptyArray<T> = {
+  0: T;
+} & Array<T>;
