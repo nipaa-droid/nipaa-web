1f6af9957a796dad210d977693978a83a7d5ce0b
o-dasher <nipaa_droid@proton.me>
2022-07-07 00:03:00 -0400

    Adds register router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -4,12 +4,14 @@ import { getRankRouter } from "./getrank";
 import { getTopRouter } from "./gettop";
 import { submitRouter } from "./submit";
 import { loginRouter } from "./login";
+import { registerRouter } from "./register";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge(getRankRouter)
   .merge(getTopRouter)
   .merge(submitRouter)
-  .merge(loginRouter);
+  .merge(loginRouter)
+  .merge(registerRouter);
 
 export type AppRouter = typeof appRouter;
