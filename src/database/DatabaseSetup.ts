9273dbf7bc2d23c62d1f70559ac4664417963ddc
o-dasher <nipaa_droid@proton.me>
2022-07-10 12:06:33 -0400

    Changes to use pp leaderboard

--- src/database/DatabaseSetup.ts
+++ src/database/DatabaseSetup.ts
@@ -1,19 +1,18 @@
 import { GameMode } from "@prisma/client";
 import { Metrics } from "./Metrics";
 
-export type ActiveGlobalLeaderboardMetric = Metrics.totalScore;
+export type ActiveGlobalLeaderboardMetric = Metrics.pp;
 
 export enum OsuDroidScoreMetric {
   score = "score",
   pp = "pp",
 }
 
-export type ActiveScoreLeaderboardMetric = OsuDroidScoreMetric.score;
+export type ActiveScoreLeaderboardMetric = OsuDroidScoreMetric.pp;
 
 export class DatabaseSetup {
   static game_mode = GameMode.std;
-  static global_leaderboard_metric: ActiveGlobalLeaderboardMetric =
-    Metrics.totalScore;
+  static global_leaderboard_metric: ActiveGlobalLeaderboardMetric = Metrics.pp;
   static score_leaderboard_metric: ActiveScoreLeaderboardMetric =
-    OsuDroidScoreMetric.score;
+    OsuDroidScoreMetric.pp;
 }
--- src/osu/GameMode.ts
+++
@@ -1,9 +0,0 @@
-/**
- * All available osu! game modes.
- */
-export enum GameMode {
-  std = 0,
-  taiko = 1,
-  mania = 2,
-  catch = 3,
-}
