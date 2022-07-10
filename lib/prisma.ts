eb5c90e6d99fa8a9ff0dcde6415ba967ecf59559
o-dasher <nipaa_droid@proton.me>
2022-07-10 13:11:08 -0400

    Adds caching from database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { minutesToMilliseconds } from "date-fns";
+import { createPrismaRedisCache } from "prisma-redis-middleware";
 
 const prisma = new PrismaClient();
+
+const cacheMiddleware = createPrismaRedisCache({
+  storage: {
+    type: "memory",
+    options: {
+      invalidation: true,
+      log: console,
+    },
+  },
+  cacheTime: minutesToMilliseconds(1),
+  onHit: (key) => console.log(`Got from cache ${key}`),
+}) as Prisma.Middleware;
+
+prisma.$use(cacheMiddleware);
 
 export { prisma };
