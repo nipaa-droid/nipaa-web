a0224c0c871d155230bc8cbdc92eab3af9761f84
o-dasher <nipaa_droid@proton.me>
2022-07-10 15:49:26 -0400

    Making the website a pwa

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+export default class _Document extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png" />
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
