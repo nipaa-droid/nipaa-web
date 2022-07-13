fe8dde965517760d22036a3c00ef57c8488bb56d
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-13 01:07:13 -0400

    Fixes logo not being loaded on lb page

--- src/components/images/AppLogo.tsx
+++ src/components/images/AppLogo.tsx
@@ -2,6 +2,6 @@ import { Image } from "@mantine/core";
 
 export const AppLogo = () => {
   return (
-    <Image alt="App logo" src="icon-512x512.webp" width={32} height={32} />
+    <Image alt="App logo" src="/icon-512x512.webp" width={32} height={32} />
   );
 };
