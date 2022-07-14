bf438d40448646f899082c1cc412b96882a1ce5f
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-14 17:39:26 -0400

    Adds session to getrank

--- src/server/routers/client/getrank.ts
+++ src/server/routers/client/getrank.ts
@@ -14,87 +14,88 @@ import {
 import { z } from "zod";
 import { shapeWithHash } from "../../shapes";
 import { ServerConstants } from "../../../constants";
+import { protectedWithSessionMiddleware } from "../../middlewares";
 
 const path = "getrank";
 
-export const clientGetRankRouter = createRouter().mutation(
-  toApiClientTrpc(path),
-  {
-    meta: {
-      openapi: {
-        enabled: true,
-        method: "POST",
-        path: toApiEndpoint(path),
-      },
+export const clientGetRankRouter = protectedWithSessionMiddleware(
+  createRouter(),
+  { id: true }
+).mutation(toApiClientTrpc(path), {
+  meta: {
+    openapi: {
+      enabled: true,
+      method: "POST",
+      path: toApiEndpoint(path),
     },
-    input: z.object({ ...shapeWithHash }),
-    output: z.string(),
-    async resolve({ input }) {
-      const { hash } = input;
+  },
+  input: z.object({ ...shapeWithHash }),
+  output: z.string(),
+  async resolve({ input }) {
+    const { hash } = input;
 
-      const responseScores: string[] = [];
+    const responseScores: string[] = [];
 
-      const end = () => {
-        return Responses.SUCCESS(responseScores.join("\n"));
-      };
+    const end = () => {
+      return Responses.SUCCESS(responseScores.join("\n"));
+    };
 
-      const beatmap = await BeatmapManager.fetchBeatmap(hash);
+    const beatmap = await BeatmapManager.fetchBeatmap(hash);
 
-      if (!beatmap) {
-        return end();
-      }
-
-      const scores = await prisma.osuDroidScore.findMany({
-        where: {
-          mapHash: hash,
-          status: {
-            in: SubmissionStatusUtils.USER_BEST_STATUS,
-          },
-        },
-        orderBy: {
-          [OsuDroidScoreHelper.getScoreLeaderboardMetricKey()]:
-            Prisma.SortOrder.desc,
-        },
-        take: ServerConstants.AMOUNT_SCORES_ON_SCORE_LEADERBOARD,
-        select: {
-          [OsuDroidScoreHelper.getScoreLeaderboardMetricKey()]: true,
-          id: true,
-          maxCombo: true,
-          h300: true,
-          h100: true,
-          h50: true,
-          h0: true,
-          mods: true,
-          player: {
-            select: {
-              name: true,
-              image: true,
-            },
-          },
-        },
-      });
-
-      scores.forEach((s) => {
-        const accPercent = OsuDroidScoreHelper.getAccuracyPercent(s);
-        const accDroid = AccuracyUtils.acc100toDroid(accPercent);
-
-        responseScores.push(
-          Responses.ARRAY(
-            s.id.toString(),
-            s.player.name,
-            Math.round(
-              OsuDroidScoreHelper.getScoreLeaderboardMetric(s)
-            ).toString(),
-            s.maxCombo.toString(),
-            OsuDroidScoreHelper.getGrade(s, { accuracy: accPercent }),
-            s.mods,
-            accDroid.toString(),
-            OsuDroidUserHelper.getImage(s.player.image)
-          )
-        );
-      });
-
+    if (!beatmap) {
       return end();
-    },
-  }
-);
+    }
+
+    const scores = await prisma.osuDroidScore.findMany({
+      where: {
+        mapHash: hash,
+        status: {
+          in: SubmissionStatusUtils.USER_BEST_STATUS,
+        },
+      },
+      orderBy: {
+        [OsuDroidScoreHelper.getScoreLeaderboardMetricKey()]:
+          Prisma.SortOrder.desc,
+      },
+      take: ServerConstants.AMOUNT_SCORES_ON_SCORE_LEADERBOARD,
+      select: {
+        [OsuDroidScoreHelper.getScoreLeaderboardMetricKey()]: true,
+        id: true,
+        maxCombo: true,
+        h300: true,
+        h100: true,
+        h50: true,
+        h0: true,
+        mods: true,
+        player: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    scores.forEach((s) => {
+      const accPercent = OsuDroidScoreHelper.getAccuracyPercent(s);
+      const accDroid = AccuracyUtils.acc100toDroid(accPercent);
+
+      responseScores.push(
+        Responses.ARRAY(
+          s.id.toString(),
+          s.player.name,
+          Math.round(
+            OsuDroidScoreHelper.getScoreLeaderboardMetric(s)
+          ).toString(),
+          s.maxCombo.toString(),
+          OsuDroidScoreHelper.getGrade(s, { accuracy: accPercent }),
+          s.mods,
+          accDroid.toString(),
+          OsuDroidUserHelper.getImage(s.player.image)
+        )
+      );
+    });
+
+    return end();
+  },
+});
