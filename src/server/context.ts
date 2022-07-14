68d211a90deda9e567c5ccd6d8c67e3d553f9404
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-14 17:51:59 -0400

    Update context.ts

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,11 +1,31 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export async function createContext({
-  req,
-  res,
-}: trpcNext.CreateNextContextOptions) {
-  return { req, res };
+const commonRequestSchema = z.object({
+  req: z.any(),
+  res: z.any(),
+});
+
+export function isCommonRequest(
+  input: unknown
+): input is { req: NextApiRequest; res: NextApiResponse<any> } {
+  return commonRequestSchema.safeParse(input).success;
+}
+
+export async function createContext(
+  options: trpcNext.CreateNextContextOptions
+) {
+  // Handles client side requests
+  if (isCommonRequest(options)) {
+    const { req, res } = options;
+    return {
+      req,
+      res,
+    };
+  }
+  return {};
 }
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
