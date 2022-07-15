010ab2e6a8d9c656b0316f4b8f68be78f3fba2fb
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-15 04:44:05 -0400

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export class ServerConstants {
   static SERVER_NAME = "Nipaa!";
   static SERVER_CREATOR = "Dasher";
-  static PRODUCTION_URL = "nipaa-web.vercel.app";
+  static PRODUCTION_URL = "nipaa.vercel.app";
   static DISCORD_URL = "https://discord.gg/CFmHRcEcz5";
   static AMOUNT_SCORES_ON_SCORE_LEADERBOARD = 50;
   static DEFAULT_AVATAR_PATH = "/avatar-guest.png";
