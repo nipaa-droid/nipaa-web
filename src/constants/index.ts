cf315b0e104d6192be0fba32699370118a1c96cc
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-17 17:38:41 -0400

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,10 +4,13 @@ export class ServerConstants {
   static PRODUCTION_URL = "nipaa.vercel.app";
   static DISCORD_URL = "https://discord.gg/CFmHRcEcz5";
   static AMOUNT_SCORES_ON_SCORE_LEADERBOARD = 50;
-  static DEFAULT_AVATAR_PATH = "/avatar-guest.png";
 
   static SERVER_URL =
     process.env.NODE_ENV === "production"
       ? `https://${ServerConstants.PRODUCTION_URL}`
       : "http://localhost:3000";
+
+  static DEFAULT_AVATAR_PATH = `${
+    process.env.NODE_ENV === "production" ? this.SERVER_URL : ""
+  }/avatar-guest.png`;
 }
