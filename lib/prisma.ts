e241e55b0e3b1b8ad1d244b93bf9f9bc3277cced
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-22 17:37:05 -0400

    Disables cache on dev

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,17 +3,19 @@ import { createPrismaRedisCache } from "prisma-redis-middleware";
 
 const prisma = new PrismaClient();
 
-const cacheMiddleware = createPrismaRedisCache({
-  storage: {
-    type: "memory",
-    options: {
-      invalidation: true,
+if (process.env.NODE_ENV === "production") {
+  const cacheMiddleware = createPrismaRedisCache({
+    storage: {
+      type: "memory",
+      options: {
+        invalidation: true,
+      },
     },
-  },
-  cacheTime: 3000,
-  onHit: (key) => console.log(`Got from cache ${key}`),
-}) as Prisma.Middleware;
+    cacheTime: 3000,
+    onHit: (key) => console.log(`Got from cache ${key}`),
+  }) as Prisma.Middleware;
 
-prisma.$use(cacheMiddleware);
+  prisma.$use(cacheMiddleware);
+}
 
 export { prisma };
