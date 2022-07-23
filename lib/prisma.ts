b2a4ace9a6a2349f3ff44a5e1b3cf63483433e21
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-07-22 21:29:08 -0400

    using redis for cache

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,24 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { createPrismaRedisCache } from "prisma-redis-middleware";
+import Redis from "ioredis";
+
+const redis = new Redis();
 
 const prisma = new PrismaClient();
 
 if (process.env.NODE_ENV === "production") {
+  const TTL = 3000;
   const cacheMiddleware = createPrismaRedisCache({
     storage: {
-      type: "memory",
+      type: "redis",
       options: {
-        invalidation: true,
+        client: redis,
+        invalidation: {
+          referencesTTL: TTL,
+        },
       },
     },
-    cacheTime: 3000,
+    cacheTime: TTL,
     onHit: (key) => console.log(`Got from cache ${key}`),
   }) as Prisma.Middleware;
 
